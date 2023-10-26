23552bcc990ad18c08c824262b20537e7dfca144
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-26 15:00:40 -0300

    feat: ai completion routes

---
+++ database/src/routes/generate-ai-completion.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { openai } from "../lib/openai";
+import { OpenAIStream, streamToResponse } from 'ai'
+
+export async function generateAiCompletionRoute(app: FastifyInstance) {
+  app.post('/ai/completion', async (request, reply) => {
+    const completionSchema = z.object({
+      prompt: z.string()
+    })
+
+    const { prompt } = completionSchema.parse(request.body)
+
+    const promptMessage = prompt.replace('{ingredientes}', prompt)
+
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      messages: [
+        { role: 'user', content: promptMessage }
+      ],
+      stream: true
+    })
+
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      }
+    })
+  })
+}
