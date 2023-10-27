3bf1df03574e26d6ae8fcb1d9fda219fbb503c8b
Matheus Motta <matheusdomingues423@gmail.com>
2023-10-27 15:46:41 -0300

    feat: registering routes

--- database/src/server.ts
+++ database/src/server.ts
@@ -1,15 +1,18 @@
 import fastify from "fastify";
-import cors from '@fastify/cors'
-import 'dotenv/config'
+import { fastifyCors } from '@fastify/cors'
 import { listAllPromptsRoute } from "./routes/list-prompts";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import { saveCompletionRoute } from "./routes/save-completion";
 
 const app = fastify()
 
-app.register(cors, {
+app.register(fastifyCors, {
   origin: '*'
 })
 
 app.register(listAllPromptsRoute)
+app.register(generateAiCompletionRoute)
+app.register(saveCompletionRoute)
 
 app
   .listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333 })
